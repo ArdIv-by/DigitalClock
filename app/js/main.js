3bf3a29299af9d36f9f95aa77c8853279cad2f05
ArdIv-by <ard.iv@yandex.ru>
2023-04-14 05:38:50 +0300

    clock js

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,24 @@
 $(function () {
-  
+  function displayTime() {
+    const dateTime = new Date();
+    const hrs = dateTime.getHours();
+    const min = dateTime.getMinutes();
+    const sec = dateTime.getSeconds();
+    const session = document.getElementById('session');
+
+    if (hrs <= 12) {
+      session.innerHTML = 'AM';
+    }else{
+      session.innerHTML = 'PM';
+    }
+
+    if (hrs > 12) {
+      hrs = hrs - 12;
+    }
+
+    document.getElementById('hours').innerHTML = hrs;
+    document.getElementById('minutes').innerHTML = min;
+    document.getElementById('seconds').innerHTML = sec;
+  }
+  setInterval(displayTime, 10);
 });
